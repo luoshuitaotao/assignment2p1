1af75fee7971560199712efd5c3ac78484a5de24
jing cui <cuijing@Jing.local>
2020-07-15 23:08:32 -0500

    add .gitignore and static

--- efs/settings.py
+++ efs/settings.py
@@ -120,7 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/'
+
 LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL='/home'
 
